4664da06ea8c1bccca54340fb1e3c00f41b2b1a6
Svitlana <svitlana.grytsai@gmail.com>
2024-03-01 15:43:41 +0100

    feature: implement stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.elements = new Array();
+  }
+
+  push(element) {
+    this.elements.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.elements.length > 0) {
+      return this.elements.pop();
+    }
+    return undefined;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const lastItemIndex = this.elements.length - 1;
+    return this.elements[lastItemIndex];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
